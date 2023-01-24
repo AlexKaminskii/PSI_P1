a14299c4401ef004b6cdcb908761886f15e50c21
Wumpusek <115797336+AlexKaminskii@users.noreply.github.com>
2023-01-24 15:31:23 +0100

    ...

--- mobile.js
+++ mobile.js
@@ -1,15 +1,23 @@
-    function menu() {
-        var button = document.getElementById("mbtn");
-        button.style.display="none";
-        var menu = document.getElementById("mobile");
-        menu.style.height="15vh"; 
-        visible();
-    }
+    // function menu() {
+    //     var button = document.getElementById("mbtn");
+    //     button.style.display="none";
+    //     var menu = document.getElementById("mobile");
+    //     menu.style.height="15vh"; 
+    //     visible();
+    // }
 
-    function visible() {
-        var container = document.getElementById("container");
-        container.style.display="block";
-    }
+    // function visible() {
+    //     var container = document.getElementById("container");
+    //     container.style.display="block";
+    // }
 
-
-    
+function menu() {
+  var x = document.getElementById("mobile");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } 
+  
+  else {
+    x.style.display = "block";
+  }
+}
